dc9b2b5edf55c76c2f6a9ec2154178357e1261c0
bwain <bwain@grubhub.com>
2016-11-08 17:48:06 -0600

    more skeleton stuff

--- app/app.js
+++ app/app.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Router, browserHistory } from 'react-router';
+import Page from './page/page.js';
 
 const App = function(){
-  return <div>App</div>;
+  return (
+    <Router history={ browserHistory }>
+      <Route path="/" component={ Page } />
+    </Router>
+  );
 };
 
 export default App;
