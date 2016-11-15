68aaf2eb82c80e6208462ad80e7635774dc05c2b
bwain <bwain@grubhub.com>
2016-11-15 00:25:49 -0600

    better scripts

--- src/components/page/page-spec.js
+++ src/components/page/page-spec.js
@@ -5,7 +5,7 @@ import SubComponent from './subcomponent/subcomponent.js';
 
 describe('Page', function(){
   it('returns a div and a subcomponent', function(){
-    expect(shallow(<Page/>).get(0)).toEqualJSX(
+    expect(shallow(<Page />).get(0)).toEqualJSX(
       <div>
         <div className="pagediv">Page</div>
         <SubComponent content="hello" />
