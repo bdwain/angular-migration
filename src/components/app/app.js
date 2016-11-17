d5bebf68185c948fa1c619a7fa88db2e5c8a6ef7
bwain <bwain@grubhub.com>
2016-11-17 00:02:47 -0600

    move things from main to app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Route, Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store.js';
+
 import Page from '../page/page.js';
+
+const store = configureStore();
 
 /* istanbul ignore next */
 const App = function(){
   return (
-    <Router history={ browserHistory }>
-      <Route path="/" component={ Page } />
-      <Route path="*" />
-    </Router>
+    <Provider store={ store }>
+      <Router history={ browserHistory }>
+        <Route path="/" component={ Page } />
+        <Route path="*" />
+      </Router>
+    </Provider>
   );
 };
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import configureStore from './components/app/store.js';
 import App from './components/app/app.js';
 
 import 'img/favicon.ico'; //until webpack has a better way to include it in the bundle
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
